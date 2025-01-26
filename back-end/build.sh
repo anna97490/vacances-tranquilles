8e2198dc23a2434af5c2300fb89db57a54a8549f
anna97490 <anna.cousin97490@gmail.com>
2025-01-26 18:23:04 +0100

    add mvn clean package cmd to build.sh

--- back-end/build.sh
+++ back-end/build.sh
@@ -3,5 +3,6 @@
 IMAGE_NAME=vacances-tranquilles/back-end
 IMAGE_TAG_CURRENT=0.0.0
 
+mvn clean package
 echo "### Build new image ${IMAGE_NAME}:${IMAGE_TAG_CURRENT}"
 docker build --no-cache --tag ${IMAGE_NAME}:${IMAGE_TAG_CURRENT} .

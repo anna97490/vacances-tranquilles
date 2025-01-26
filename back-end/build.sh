35cf66839c5b577584ffc5f42e399a025db734fe
anna97490 <anna.cousin97490@gmail.com>
2025-01-26 17:50:38 +0100

    init docker compose

---
+++ back-end/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IMAGE_NAME=vacances-tranquilles/back-end
+IMAGE_TAG_CURRENT=0.0.0
+
+echo "### Build new image ${IMAGE_NAME}:${IMAGE_TAG_CURRENT}"
+docker build --no-cache --tag ${IMAGE_NAME}:${IMAGE_TAG_CURRENT} .
--- back-end/src/test/java/com/mastere_project/vacances_tranquilles/VacancesTranquillesApplicationTests.java
+++ back-end/src/test/java/com/mastere_project/vacances_tranquilles/VacancesTranquillesApplicationTests.java
@@ -3,7 +3,9 @@ package com.mastere_project.vacances_tranquilles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class VacancesTranquillesApplicationTests {
 
 	@Test
